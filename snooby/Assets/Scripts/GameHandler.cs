cd14b38ef6fe9306d31a25796584564fba5e1a73
bboybrian <tanszekai@gmail.com>
2021-12-14 17:52:32 +0800

    forgot a semicolon

--- snooby/Assets/Scripts/GameHandler.cs
+++ snooby/Assets/Scripts/GameHandler.cs
@@ -7,7 +7,7 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("GameHandler started")
+        Debug.Log("GameHandler started");
     }
 
     // Update is called once per frame
