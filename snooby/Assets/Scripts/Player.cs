1a3d40c3938a16fee884a7b7c8fb32f80b2cd4df
bboybrian <tanszekai@gmail.com>
2022-01-03 17:17:31 +0800

    yeni turn

--- snooby/Assets/Scripts/Player.cs
+++ snooby/Assets/Scripts/Player.cs
@@ -25,17 +25,29 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Jump");
-            rb.velocity = new Vector2(0.0f, 2.0f);
+            changeVely(new Vector2(0.0f, 2.0f));
         }
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Left");
-            rb.velocity = new Vector2(-2.0f, 0.0f);
+            changeVelx(new Vector2(-2.0f, 0.0f));
         }
         if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Right");
-            rb.velocity = new Vector2(2.0f, 0.0f);
+            changeVelx(new Vector2(2.0f, 0.0f));
+        }
+    }
+    void changeVelx(Vector2 v)
+    {
+        if (rb.velocity[0] < 3 && rb.velocity[0] > -3){
+            rb.velocity = rb.velocity + v;
+        }
+    }
+    void changeVely(Vector2 v)
+    {
+        if (rb.velocity[1] == 0){
+            rb.velocity = rb.velocity + v;
         }
     }
 }
