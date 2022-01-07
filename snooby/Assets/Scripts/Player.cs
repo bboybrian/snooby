a2c4b9f016821a9e7c5f7c95734032477f89a620
bboybrian <tanszekai@gmail.com>
2022-01-07 18:54:56 +0800

    basic

--- snooby/Assets/Scripts/Player.cs
+++ snooby/Assets/Scripts/Player.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rb;
+    private Rigidbody2D rb;
+    private float moveSpeed = 2.0f;
+    private float jumpSpeed = 5.0f;
 
     void Awake()
     {
         rb = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
         rb.bodyType = RigidbodyType2D.Dynamic;
+        rb.freezeRotation = true;
     }
 
     // Start is called before the first frame update
@@ -21,33 +24,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // rb.velocity = new Vector2(0, -9.8f);
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Jump");
-            changeVely(new Vector2(0.0f, 5.0f));
+            rb.velocity = new Vector2(0.0f, jumpSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Left");
-            changeVelx(new Vector2(-2.0f, 0.0f));
+            rb.velocity = new Vector2(-moveSpeed, 0.0f);
         }
         if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Right");
-            changeVelx(new Vector2(2.0f, 0.0f));
-        }
-    }
-    void changeVelx(Vector2 v)
-    {
-        if (rb.velocity[0] < 3 && rb.velocity[0] > -3){
-            rb.velocity = rb.velocity + v;
-        }
-    }
-    void changeVely(Vector2 v)
-    {
-        if (rb.velocity[1] == 0){
-            rb.velocity = rb.velocity + v;
+            rb.velocity = new Vector2(moveSpeed, 0.0f);
         }
     }
 }
